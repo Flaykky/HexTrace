3b78de28fe3b904ec9071c31ba4662252cd03db6
flayk <138144836+Flaykky@users.noreply.github.com>
2025-02-14 23:04:22 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,8 @@
 #include "disAssembler.h"
 #include <iostream>
 #include <string>
-
-
-
-size_t calculateSize(const char* file_path) {
-    FILE* file = fopen(file_path, "rb");
-    if (!file) {
-        fprintf(stderr, "Error: Failed to open file %s\n", file_path);
-        return 0;
-    }
-    
-    fseek(file, 0, SEEK_END);
-    long size = ftell(file);
-    fseek(file, 0, SEEK_SET);
-    
-    fclose(file);
-    
-    return size;
-}
-
+#include <utils.c>
+#include "utils.h"
 
 
 
