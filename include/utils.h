01b9c559be3cb244e2d5a543005145d0b9ed6b0a
flayk <138144836+Flaykky@users.noreply.github.com>
2025-02-16 19:56:38 +0300

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,10 +1,24 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-// обьявление функций 
 
-size_t calculateSize(const char* file_path);
+#include <basetsd.h> // для size_t
 
+
+// ======== структуры ========
+
+// пока нету
+
+
+// ======== обьявление функций ========
+
+size_t calculateSize(const char* file_path); // вычисление размера файла 
+    
+char* readBinaryFile(const char* file_path, size_t* file_size); // чтение бинарного файла 
+    
+void createDisAsmFile(const char* asmCode); // создания дизассемблированного файла 
+
+// readBinaryFile должна возвращать чтото типо такого: FFA1, тоесть двоичный код в шестнадцатеричном формате
 
 
 #endif 
