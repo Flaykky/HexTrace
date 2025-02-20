6902c0104336981c98a498bd80d0b0f93fc55599
flayk <138144836+Flaykky@users.noreply.github.com>
2025-02-20 19:25:01 +0300

    Update disAssembler.h

--- include/disAssembler.h
+++ include/disAssembler.h
@@ -1,35 +1,14 @@
-#ifndef disAssembler_H
-#define disAssembler_H
+#ifndef DISASSEMBLER_H
+#define DISASSEMBLER_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// ======= структуры ======= 
-
-typedef struct {
-    char* buffer;
-    int size;
-} BinaryFile;
-
-typedef struct {
-    char* buffer;
-    int size;
-} AsmFile;
-
-#include <sys/types.h>
-#include <baseTsd.h>
-
-
-// ======= обьявление функций =======
-
-// основная функция для дизассемблера
-char* disAssembler(const char* binaryCode, size_t codeSize, uint64_t address);
-// должна принимать чтото типо такого: 0x55, 0x48, 0x89, 0xe5, а возвращать push rbp mov rbp, rsp и тд 
-
+char* disassemble(const char* binaryCode, size_t codeSize, uint64_t address);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif 
+#endif
